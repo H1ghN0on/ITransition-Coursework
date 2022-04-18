2ef997a85dd6dcabee517758dc0893839b666f90
Igor <devNoon@yandex.ru>
2022-04-18 17:32:41 +0300

    Tailwind css connected

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>Hello, world</div>;
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen">
+      <h1 className="text-3xl">Hello, world</h1>
+    </div>
+  );
 };
 
 export default Home;
